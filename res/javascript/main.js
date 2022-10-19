aa4cfc5db3c6905a5502cab2dd93f32fca32ba00
VojtaKdo <Atjov05@seznam.cz>
2022-10-19 08:40:02 +0200

    ini

---
+++ res/javascript/main.js
@@ -0,0 +1,43 @@
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+const autoclickerUpgrade = document.getElementById("autoclickerUpgrade");
+
+let cookies = 0;
+let clickIncrease = 1;
+let clickIncreasePrice = 100;
+
+let autoclickerIncrease = 0; 
+let autoclickerPrice = 200; //cena
+
+cookie.onclick = () => {
+  cookies += clickIncrease; //zvedne o jednu
+  counter.innerHTML = cookies;
+};
+
+clickUpgrade.onclick = () => {
+    if(cookies >= clickIncreasePrice){
+    cookies -= clickIncreasePrice;
+    clickIncreasePrice += 100;
+    clickUpgrade.innerHTML = `Buy Upgrade: ${clickIncreasePrice}`;
+    counter.innerHTML = cookies;
+    clickIncrease++;
+    }
+    
+}
+
+autoclickerUpgrade.onclick = () => {
+    if(cookies >= autoclickerPrice){
+        cookies -= autoclickerPrice;
+        counter.innerHTML = cookies;
+        autoclickerPrice += 100;
+        autoclickerUpgrade.innerHTML = `Buy autoclicker upgrade: ${autoclickerPrice}`;
+    }
+    if(autoclickerIncrease==0){
+        setInterval(() => {
+            cookies += autoclickerIncrease; //zvedne o jednu
+            counter.innerHTML = cookies;
+        }, 0.1); // opakovani kazdou sekundu
+    }
+    autoclickerIncrease++;
+}
